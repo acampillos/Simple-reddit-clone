9a6bb894081115bbeb4d8a4e2cddbbd14ad0b207
acampillos <a.campillos.delgado@gmail.com>
2021-07-26 13:36:31 +0200

    Initial tests config

--- src/test/kotlin/com/practice/redditclone/RedditCloneApplicationTests.kt
+++ src/test/kotlin/com/practice/redditclone/RedditCloneApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.practice.redditclone
 
+import com.practice.redditclone.dto.RegisterRequest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RedditCloneApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @LocalServerPort
+    var port: Int = 0
+
+    @Test
+    fun contextLoads() {
+    }
+
+
 
 }
