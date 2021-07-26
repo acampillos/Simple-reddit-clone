4ac5c16aaec538f587968794bbf1cebcaf925804
acampillos <a.campillos.delgado@gmail.com>
2021-07-26 13:22:44 +0200

    Add JWT dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,16 +21,21 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.5.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("io.github.microutils:kotlin-logging:1.12.5")
 	implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final")
+	implementation("io.github.microutils:kotlin-logging:1.12.5")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
 	runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.5.2")
 
 }
 
